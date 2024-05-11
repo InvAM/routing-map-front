1e7692e3b2451af9fad5d43ca6b42d2cdfc0b942
fpachasl <famos@pattern.pe>
2024-05-11 16:59:18 -0500

    update commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -77,77 +77,57 @@
 						{{ eco.apellido }}
 					</a>
 				</v-row>
+				<v-row>
+					<v-col>
+						<v-btn color="primary" @click="enviarInformacion">
+							Enviar Información
+						</v-btn>
+					</v-col>
+				</v-row>
 			</v-col>
 		</v-row>
 	</v-container>
 </template>
-
 <script>
+	import axios from "axios";
+
 	export default {
 		name: "HelloWorld",
 
 		data: () => ({
-			ecosystem: [
-				{
-					text: "vuetify-loader",
-					href: "https://github.com/vuetifyjs/vuetify-loader/tree/next",
-				},
-				{
-					text: "github",
-					href: "https://github.com/vuetifyjs/vuetify/tree/next",
-				},
-				{
-					text: "awesome-vuetify",
-					href: "https://github.com/vuetifyjs/awesome-vuetify",
-				},
-			],
-			importantLinks: [
-				{
-					text: "Chat",
-					href: "https://community.vuetifyjs.com",
-				},
-				{
-					text: "Made with Vuetify",
-					href: "https://madewithvuejs.com/vuetify",
-				},
-				{
-					text: "Twitter",
-					href: "https://twitter.com/vuetifyjs",
-				},
-				{
-					text: "Articles",
-					href: "https://medium.com/vuetify",
-				},
-			],
-			whatsNext: [
-				{
-					text: "Explore components",
-					href: "https://vuetifyjs.com",
-				},
-				{
-					text: "Roadmap",
-					href: "https://vuetifyjs.com/introduction/roadmap/",
-				},
-				{
-					text: "Frequently Asked Questions",
-					href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-				},
-			],
 			nombres: [
-				{
-					text: "Miguel",
-					apellido: "Arone",
-					href: "Menu",
-				},
-				{
-					text: "Freddy",
-					apellido: "Pachas",
-				},
-				{
-					text: "Carlos",
-					apellido: "Alcedo",
-				},
+				{ text: "Miguel", apellido: "Arone" },
+				{ text: "Freddy", apellido: "Pachas" },
+				{ text: "Carlos", apellido: "Alcedo" },
 			],
 		}),
+
+		methods: {
+			enviarInformacion() {
+				if (!this.nombres || this.nombres.length === 0) {
+					alert("No hay datos para enviar.");
+					return;
+				}
+
+				const url = "http://localhost:8000/ia/";
+				const data = {
+					nombres: this.nombres.map((persona) => ({
+						nombre: persona.text,
+						apellido: persona.apellido,
+					})),
+				};
+				axios
+					.post(url, data)
+					.then((response) => {
+						alert(
+							"Información enviada correctamente: " +
+								JSON.stringify(response.data)
+						);
+					})
+					.catch((error) => {
+						alert("Error al enviar la información: " + error.message);
+					});
+			},
+		},
 	};
 </script>
